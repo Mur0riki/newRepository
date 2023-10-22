c7a66c1130c5bc9c6dda450c0e105f327a44ce40
Mur0riki <nevzlomaite2283@mail.ru>
2023-10-22 05:12:38 +0300

    Немного исправлен стиль написания

--- project-template/src/main/java/edu/hw2/Task1.java
+++ project-template/src/main/java/edu/hw2/Task1.java
@@ -1,4 +1,43 @@
 package edu.hw2;
 
 public class Task1 {
+    public sealed interface Expr {
+        double evaulate();
+
+        public record Constant(double value) implements Expr {
+            @Override
+            public double evaulate() {
+                double result = this.value;
+                return result;
+            }
+        }
+
+        public record Negate(Constant value) implements Expr  {
+
+            @Override
+            public double evaulate() {
+                return (-1*value.evaulate());
+            }
+        }
+
+        public record Exponent(Constant value, double exponent) implements Expr{
+            @Override
+            public double evaulate(){
+                return Math.pow(value.evaulate(),exponent);
+            }
+        }
+        public record Addition(Constant firstValue, Constant secondValue) implements Expr{
+
+            @Override
+            public double evaulate() {
+                return firstValue.evaulate()+secondValue.evaulate();
+            }
+        }
+        public record Multiplication(Constant firstValue, Constant secondValue) implements Expr {
+            @Override
+            public double evaulate() {
+                return firstValue.evaulate()*secondValue.evaulate();
+            }
+        }
+    }
 }
--- project-template/src/main/java/edu/project1/HangmanGame.java
+++ project-template/src/main/java/edu/project1/HangmanGame.java
@@ -3,22 +3,63 @@ package edu.project1;
 import java.util.Scanner;
 
 public class HangmanGame {
-    Player player;
-    String word;
 
-    HangmanGame(Player player, String guessedWord) {
-        this.player = player;
-        this.word = guessedWord;
-    }
+    private static final String[] WORDS = {"apple", "banana", "cherry", "date", "elderberry"};
+    private static final int MAX_TRIES = 6;
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите слово которое нужно будет угадывать.");
-        Board board = new Board(in.nextLine());
-        RandomPlayer player = new RandomPlayer();
-        ConsoleHangman session = new ConsoleHangman(player,board);
-        while (board.stillPlayable()){
-            session.play();
+
+        String secretWord = getRandomWord();
+        char[] guessedLetters = new char[secretWord.length()];
+        int tries = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Добро пожаловать в игру \"Виселица\"!Если вы желаете сдаться преждевременно закончив игру, напишите \"exit\"");
+
+        while (tries < MAX_TRIES) {
+            System.out.print("Угадайте букву или слово: ");
+            String guess = scanner.nextLine();
+            if(guess.equals("exit")){
+                break;
+            }
+            if (guess.length() == 1) {
+                char letter = guess.charAt(0);
+                boolean found = false;
+
+                for (int i = 0; i < secretWord.length(); i++) {
+                    if (secretWord.charAt(i) == letter) {
+                        guessedLetters[i] = letter;
+                        found = true;
+                    }
+                }
+
+                if (found) {
+                    System.out.println("Поздравляю, вы угадали букву!");
+                } else {
+                    tries++;
+                    System.out.println("Упс, неправильная буква. Осталось попыток: " + (MAX_TRIES - tries));
+                }
+
+            } else {
+                System.out.println("Неверный ввод. Пожалуйста, введите одну букву.");
+            }
+
+            System.out.println("Угаданные буквы: " + String.valueOf(guessedLetters));
+
+            if (String.valueOf(guessedLetters).equalsIgnoreCase(secretWord)) {
+                System.out.println("Поздравляю, вы выиграли! Загаданное слово: " + secretWord);
+                break;
+            }
         }
+
+        if (tries == MAX_TRIES) {
+            System.out.println("Игра окончена! Вы проиграли. Загаданное слово: " + secretWord);
+        }
+
+        scanner.close();
+    }
+
+    private static String getRandomWord() {
+        return WORDS[(int) (Math.random() * WORDS.length)];
     }
 }
