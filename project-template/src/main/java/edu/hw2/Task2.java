bc0874098becac925af65a7241cba57a5cd733b7
Mur0riki <nevzlomaite2283@mail.ru>
2023-10-25 02:16:37 +0300

    Task1: Убрал модификатор доступа в внутренних классов с public на package-private Укоротил вычисление отрицательных выражений Task2: Убрал модификатор доступа в внутренних классов с public на package-private Добавил сеттеры для прямоугольника по одной стороне Task3: Убрал модификатор доступа в внутренних классов с public на package-private Исправил ошибку из-за котройо faultyconnection всегда кидало исключение, теперь с вероятностью 50% будет выкидываться исключение

--- project-template/src/main/java/edu/hw2/Task2.java
+++ project-template/src/main/java/edu/hw2/Task2.java
@@ -2,11 +2,11 @@ package edu.hw2;
 
 public class Task2 {
 
-    public static class Rectangle {
+    static class Rectangle {
         private final int width;
         private final int height;
 
-        public Rectangle(int width, int height){
+        public Rectangle(int width, int height) {
             this.width = width;
             this.height = height;
         }
@@ -16,22 +16,30 @@ public class Task2 {
             this.height = 0;
         }
 
+        public Rectangle setWidth(int width) {
+            return new Rectangle(height, width);
+        }
+
+        public Rectangle setHeight(int height) {
+            return new Rectangle(height, width);
+        }
+
         public double area() {
             return width * height;
         }
     }
 
-    public static class Square extends Rectangle {
+    static class Square extends Rectangle {
         private final int width = 0;
         private final int height = 0;
         private final int size;
 
-        Square(int size){
+        Square(int size) {
             this.size = size;
         }
 
         @Override public double area() {
-            return size*size;
+            return size * size;
         }
 
     }
